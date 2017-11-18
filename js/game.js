a0b0e06eabe0de4b6d6cfc5e48b5eebc316ec26b
Weber Mori <webermori@gmail.com>
2017-11-18 18:15:40 -0200

    Animação do personagem.

--- js/game.js
+++ js/game.js
@@ -5,12 +5,15 @@ var game = new Phaser.Game(800, 640,Phaser.AUTO,'',{
 });
 
 var plataformas;
+var personagem;
+var tecla;
 
 function preload() {
     game.load.image('ceu','sprites/sky.png');
     game.load.image('chao','sprites/platform.png');
     game.load.image('estrela','sprites/star.png');
     game.load.spritesheet('personagem', 'sprites/dude.png', 32,48);
+    tecla = game.input.keyboard.createCursorKeys();
 }
 
 function create() {
@@ -34,13 +37,31 @@ function create() {
     game.physics.arcade.enable(personagem);
 
     personagem.body.bounce.y = 0.5;
-    personagem.body.gravity.y = 600;
+    personagem.body.gravity.y = 300;
     personagem.body.collideWorldBounds = true;
+
+    personagem.animations.add('esquerda', [0,1,2,3], 10, true);
+    personagem.animations.add('direita', [5,6,7,8], 10, true);
 
 }
 
 function update() {
     var noChao = game.physics.arcade.collide(personagem, plataformas);
+    
+    personagem.body.velocity.x = 0;
 
+    if (tecla.right.isDown) {
+        personagem.body.velocity.x = 400;
+        personagem.animations.play('direita');
+    } else if(tecla.left.isDown) {
+        personagem.body.velocity.x = -400;
+        personagem.animations.play('esquerda');
+    } else {
+        personagem.animations.stop();
+        personagem.frame = 4;
+    }
 
+    if (tecla.up.isDown && noChao && personagem.body.touching.down) {
+        personagem.body.velocity.y = -400;
+    }
 }
